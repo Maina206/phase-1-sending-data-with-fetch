43326f72bb7c732e2bb7a72af5ec127995917500
Alexander <maina206mwangi@gmail.com>
2024-10-13 21:19:22 +0300

    sending data with fetch complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const data = {
+      name: userName,
+      email: userEmail,
+    };
+  
+    return fetch('http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(function (data) {
+        document.body.innerHTML += `<p>${data.id}</p>`
+        console.log(data.id)
+    })
+    .catch(error => {
+        document.body.innerHTML += `<p>${error.message}</p>`;
+      });
+  }
+
+  submitData("Steve", "steve@steve.com")
